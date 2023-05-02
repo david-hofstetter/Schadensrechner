eb55c41070a42fef1124b3ec1b23400d4f224db9
david-hofstetter <david.hofstetter@lernende.bbw.ch>
2023-05-02 19:29:48 +0200

    router hinzugefügt

---
+++ src/Calculator.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+
+export default function Calculator() {
+    const [baseDamage, setBaseDamage] = useState();
+    const [damageModifier, setDamageModifier] = useState();
+    const [totalDamage, setTotalDamage] = useState();
+  
+    const calculateDamage = () => {
+      const calculatedDamage = baseDamage * damageModifier / 100 ;
+      setTotalDamage(calculatedDamage);
+    };
+  
+    return (
+      <div>
+        <h1>Schadensrechner</h1>
+        <label>
+          Grundschaden:
+          <input
+          placeholder="in CHF"
+            type="number"
+            value={baseDamage}
+            onChange={(e) => setBaseDamage(parseInt(e.target.value))}
+          />
+        </label>
+        <br />
+        <label>
+          Schadensmodifikator:
+          <input
+            placeholder="in %"
+            type="number"
+            step="5"
+            value={damageModifier}
+            onChange={(e) => setDamageModifier(parseFloat(e.target.value))}
+          />
+        </label>
+        <br />
+        <button onClick={calculateDamage}>Berechnen</button>
+        <br />
+        <label>Schadensersatzbetrag: {totalDamage}</label>
+      </div>
+    );
+  }
+  
+  //export default Calculator;
